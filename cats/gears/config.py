763cc5df5d346a8556904a6fadc219a72ca1cdd2
shotowon <sanzhar.maratov@proton.me>
2024-11-22 22:40:23 +0500

    update: define config classes

--- cats/gears/config.py
+++ cats/gears/config.py
@@ -1,7 +1,17 @@
 from enum import Enum
+from pydantic import BaseModel, Field
 
 
 class EnvConsts(str, Enum):
     LOCAL = 'local'
     DEV = 'dev'
     PROD = 'prod'
+class HTTPServerConfig(BaseModel):
+    host: str = Field(default='127.0.0.1')
+    port: int = Field(default=6810)
+
+
+class Config(BaseModel):
+    env: EnvConsts
+    storage_name: str = Field(default='storage/local.db')
+    http_server: HTTPServerConfig
